ba0869d6093994ddcb4924b88e91720b5127ce65
Manabendu-ai <rikugithub@gmail.com>
2026-08-28 03:08:04 +0530

    CREATE A PERSON TABLE without constrains

--- Chapter1/basics2.sql
+++ Chapter1/basics2.sql
@@ -9,4 +9,11 @@
   )
 
  */
-
+-- CREATE A PERSON TABLE without constrains
+CREATE TABLE person(
+    id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(6),
+    date_of_birth DATE
+);
