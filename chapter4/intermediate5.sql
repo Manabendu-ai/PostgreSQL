60fc2cf1bc84e51daf48a7ff1e91ca9a19352fb6
Manabendu-ai <rikugithub@gmail.com>
2026-08-30 14:56:29 +0530

    intermediate5.sql

--- chapter4/intermediate5.sql
+++ chapter4/intermediate5.sql
@@ -6,5 +6,6 @@ SELECT EXTRACT(DAY FROM NOW()); -- Extracting the actual DAY
 SELECT EXTRACT(CENTURY FROM NOW()); -- Extracting the actual CENTURY
 
 -- AGE:
-SELECT first_name, last_name, date_of_birth, EXTRACT(YEARS FROM AGE(date_of_birth)) as Age
+SELECT first_name, last_name, date_of_birth,
+       EXTRACT(YEARS FROM AGE(date_of_birth)) as Age
 FROM person;
