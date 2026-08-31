0cde52ee17f8082e1d1c13396cc4b760d782d2f2
Manabendu-ai <rikugithub@gmail.com>
2026-08-31 13:23:15 +0530

    intermediate1.sql

--- chapter6/intermediate1.sql
+++ chapter6/intermediate1.sql
@@ -9,3 +9,12 @@ SET price = price+ (price * 0.05)
 WHERE price >= 50000.0;
 
 SELECT * FROM car;
+
+-- Let's add an age column in the person table and update with their respective ages.
+ALTER TABLE person
+ADD COLUMN age INTEGER;
+
+UPDATE person
+SET age = EXTRACT(YEARS FROM AGE(date_of_birth)); --here we have not used where clause hence it will update all the rows.
+
+SELECT * FROM person;
