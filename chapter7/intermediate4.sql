7905620cb516c099a33db6dab02e6badc8d8560a
Manabendu-ai <rikugithub@gmail.com>
2026-08-31 15:24:10 +0530

    intermediate4.sql

---
+++ chapter7/intermediate4.sql
@@ -0,0 +1,8 @@
+/**
+  Left joins : It is a Join that returns all the records in the left table (including the non-foreign key ones)
+  and those records from the right table which has a link on the left table.
+ */
+
+SELECT * FROM person
+LEFT JOIN car
+ON person.car_id = car.id;
