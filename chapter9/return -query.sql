cd65517e9eb5256cd4ceffc48b275c99f0f5421e
Manabendu-ai <rikugithub@gmail.com>
2026-09-01 15:34:29 +0530

    get people by birth year function with a return query

--- chapter9/return -query.sql
+++ chapter9/return -query.sql
@@ -4,3 +4,26 @@
 
 -- Let's build a function that takes in year and return the people who were born in that particular year.
 
+CREATE OR REPLACE FUNCTION getPeopleByBirthYear(yr INT)
+RETURNS  TABLE(
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+)
+AS
+    $$
+    BEGIN
+        RETURN QUERY
+        SELECT person.first_name,
+               person.last_name,
+               person.email
+        FROM
+            person
+        WHERE
+            EXTRACT(YEAR FROM person.date_of_birth)::INT = yr;
+    END;
+    $$
+LANGUAGE plpgsql;
+
+SELECT * FROM getPeopleByBirthYear(2003);
+
