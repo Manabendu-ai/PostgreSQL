1b51bb7173590928a469f4beabce1c347bd84949
Manabendu-ai <rikugithub@gmail.com>
2026-09-01 13:38:35 +0530

    If-Else theory

---
+++ chapter9/if-else.sql
@@ -0,0 +1,16 @@
+/**
+  PostgreSQL allows us to write if else condition in the SQL directly.
+  It always u to check certain condition on the series of data and let's you to compute something out of it.
+ */
+
+/**
+  syntax:
+  IF <condition> THEN
+        <statements>
+  ELSIF <condition> THEN
+        <statements>
+  ELSEIF <condition> THEN
+        <statements>
+  ELSE
+        <statement>
+ */
