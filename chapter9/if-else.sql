25ee14b2bc56cb27a43d90e942dd936bc9d5415a
Manabendu-ai <rikugithub@gmail.com>
2026-09-01 13:44:38 +0530

    Let's create a function that assign grades based on the marks

--- chapter9/if-else.sql
+++ chapter9/if-else.sql
@@ -14,3 +14,27 @@
   ELSE
         <statement>
  */
+
+-- Let's create a function that assign grades based on the marks:
+
+CREATE OR REPLACE FUNCTION grade(marks DOUBLE PRECISION)
+RETURNS VARCHAR
+AS
+    $$
+        BEGIN
+            IF marks >= 90.0 THEN
+                RETURN 'A+';
+            ELSIF marks < 90.0 AND marks >= 80.0 THEN
+                RETURN 'A';
+            ELSIF marks < 80.0 AND marks >= 70.0 THEN
+                RETURN 'B';
+            ELSIF marks < 70.0 AND marks >= 55.0 THEN
+                RETURN 'C';
+            ELSIF marks < 55.0 AND marks >= 40.0 THEN
+                RETURN 'D';
+            ELSE
+                RETURN 'F';
+            END IF;
+        END;
+    $$
+LANGUAGE plpgsql;
