416ada07e5435916a5a4438809b7cff08f1a1bfb
Manabendu-ai <rikugithub@gmail.com>
2026-09-01 14:27:32 +0530

    -- let's create a function that swaps two numbers

--- chapter9/detailed-parameter-types.sql
+++ chapter9/detailed-parameter-types.sql
@@ -1,1 +1,12 @@
 -- parameter type {in*|out|inout|VARIADIC**} *default **variable number of arguments
+
+-- let's create a function that swaps two numbers
+
+CREATE OR REPLACE FUNCTION swap(INOUT num1 INT, INOUT num2 INT) -- we are not returning anything
+AS
+    $$
+        BEGIN
+            SELECT num1, num2 INTO num2, num1; -- swapping logic
+        END;
+    $$
+LANGUAGE plpgsql;
