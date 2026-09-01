913f65f9164200b067fc5ac801c80eb4c59d651b
Manabendu-ai <rikugithub@gmail.com>
2026-09-01 15:19:59 +0530

    Let's build a function that takes in year and return the people who were born in that particular year.

---
+++ chapter9/return -query.sql
@@ -0,0 +1,6 @@
+/**
+  In this will learn how to return a table as from a function
+ */
+
+-- Let's build a function that takes in year and return the people who were born in that particular year.
+
