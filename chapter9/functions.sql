9507e2a9b8b86c8951d1c1173a1ce458b077317b
Manabendu-ai <rikugithub@gmail.com>
2026-09-01 12:57:10 +0530

    functions theory 1

---
+++ chapter9/functions.sql
@@ -0,0 +1,33 @@
+/**
+
+  Functions:
+  In PostgreSQl creating a function allows you to group and execute a series of data operations directly
+  on SQL server.
+  The main use of function is to encapsulate business logic, improve query performance and ensure code reusability.
+
+  syntax:
+  CREATE [ OR REPLACE ] FUNCTION
+    name ( [ [ argmode ] [ argname ] argtype [ { DEFAULT | = } default_expr ] [, ...] ] )
+    [ RETURNS rettype
+      | RETURNS TABLE ( column_name column_type [, ...] ) ]
+  { LANGUAGE lang_name
+    | TRANSFORM { FOR TYPE type_name } [, ... ]
+    | WINDOW
+    | { IMMUTABLE | STABLE | VOLATILE }
+    | [ NOT ] LEAKPROOF
+    | { CALLED ON NULL INPUT | RETURNS NULL ON NULL INPUT | STRICT }
+    | { [ EXTERNAL ] SECURITY INVOKER | [ EXTERNAL ] SECURITY DEFINER }
+    | PARALLEL { UNSAFE | RESTRICTED | SAFE }
+    | COST execution_cost
+    | ROWS result_rows
+    | SUPPORT support_function
+    | SET configuration_parameter { TO value | = value | FROM CURRENT }
+    | AS 'definition'
+    | AS 'obj_file', 'link_symbol'
+    | sql_body
+  } ...
+
+  simple syntax:
+
+
+ */
