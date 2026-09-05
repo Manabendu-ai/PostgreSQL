541b25c2eae5a6aa09fa45e8dedd00a4e800a186
Manabendu-ai <rikugithub@gmail.com>
2026-09-05 19:38:06 +0530

    -- Let's find the employees whose salary is more than the avg salary of the employee table;

--- chapter10/sub-query1.sql
+++ chapter10/sub-query1.sql
@@ -3,3 +3,14 @@
  Sub Query is SQL query which is placed inside another query.
  */
 
+SELECT * FROM Employee;
+
+-- Let's find the employees whose salary is more than the avg salary of the employee table;
+
+SELECT AVG(salary) FROM Employee;
+
+SELECT *
+FROM Employee
+WHERE salary > (
+        SELECT AVG(salary) FROM Employee
+);
