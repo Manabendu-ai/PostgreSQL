33b5d022bec325aeeaab95d13385acfbd33764ee
Manabendu-ai <rikugithub@gmail.com>
2026-09-05 20:07:17 +0530

    -- Let's find the employees who earn the highest salary in each dept;

--- chapter10/multiple-row-subquery.sql
+++ chapter10/multiple-row-subquery.sql
@@ -9,6 +9,15 @@
 -- Let's find the employees who earn the highest salary in each dept;
 
 -- highest salary in each dept.
-SELECT MAX(salary) AS highest_salary, dept
+SELECT dept, MAX(salary) AS highest_salary
 FROM Employee
 GROUP BY dept;
+
+-- Actual query to get the details of all the employees as well:
+SELECT *
+FROM Employee
+WHERE (dept, salary) in (
+        SELECT dept, MAX(salary)
+        FROM Employee
+        GROUP BY dept
+    );
