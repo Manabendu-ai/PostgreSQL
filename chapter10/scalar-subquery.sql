84f21736c7d6fa54ed28107fb4bb21658d7ecb7d
Manabendu-ai <rikugithub@gmail.com>
2026-09-05 19:49:45 +0530

    We can write the above query in a different manner as well using JOIN clause

--- chapter10/scalar-subquery.sql
+++ chapter10/scalar-subquery.sql
@@ -9,3 +9,18 @@ WHERE salary > (
         SELECT AVG(salary)
         FROM Employee
     );
+
+/*
+ In the above query sample:
+        SELECT AVG(salary)
+        FROM Employee
+    This is a scalar query as it always returns us a single record!
+    SQL kinda treat it as a separate table as well.
+ */
+
+-- We can write the above query in a different manner as well using JOIN clause
+
+SELECT e.*
+FROM Employee e
+JOIN (SELECT AVG(salary) sal FROM Employee) avg_sal
+ON e.salary > avg_sal.sal;
