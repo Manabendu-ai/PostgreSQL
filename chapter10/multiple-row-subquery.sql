038d00175e0f8cea3441ef1fc4ce0e82c77f6cbb
Manabendu-ai <rikugithub@gmail.com>
2026-09-05 20:05:12 +0530

    -- highest salary in each dept.

--- chapter10/multiple-row-subquery.sql
+++ chapter10/multiple-row-subquery.sql
@@ -5,3 +5,10 @@
   one that returns multiple rows and multiple cols.
   other that return 1 col but multiple rows.
  */
+
+-- Let's find the employees who earn the highest salary in each dept;
+
+-- highest salary in each dept.
+SELECT MAX(salary) AS highest_salary, dept
+FROM Employee
+GROUP BY dept;
