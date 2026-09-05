7a9abffb018b42b1d416ea1a7697752c8e17ee9a
Manabendu-ai <rikugithub@gmail.com>
2026-09-05 19:53:27 +0530

    Multiple Row Subquery:

---
+++ chapter10/multiple-row-subquery.sql
@@ -0,0 +1,7 @@
+/**
+  Multiple Row Subquery:
+  It is the subquery that returns multiple records.
+  There are two types of it:
+  one that returns multiple rows and multiple cols.
+  other that return 1 col but multiple rows.
+ */
